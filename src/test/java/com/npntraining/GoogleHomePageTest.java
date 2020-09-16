dfe90cfa844f03098d6b18ab2d3b4c95bae00c92
Naveen <npn.npntraining@gmail.com>
2020-09-17 00:22:18 +0530

    Sample project for Jenkins

---
+++ src/test/java/com/npntraining/GoogleHomePageTest.java
@@ -0,0 +1,29 @@
+package com.npntraining;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleHomePageTest {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUpBeforeClass() {
+		System.setProperty("webdriver.gecko.driver", "e:\\selenium-jars\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+
+	@Test
+	public void testHomePage() {
+		driver.get("http://www.google.co.in");
+		Assert.assertEquals(driver.getTitle(), "Google");
+	}
+
+	@AfterClass
+	public void tearDownAfterClass() {
+		driver.quit();
+	}
+}
